6d68e57b903207ac16cb44393ee28b5388ece13c
relive <relive27@163.com>
2021-11-21 21:11:35 +0800

    feat:spring配置flyway

---
+++ spring-boot-flyway/src/main/java/com/relive/FlywayApplication.java
@@ -0,0 +1,15 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ReLive
+ * @Date 2020/9/29-11:15
+ */
+@SpringBootApplication
+public class FlywayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApplication.class,args);
+    }
+}
---
+++ spring-boot-flyway/src/main/resources/db/migrations/V1.1__create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `db_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(18) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
