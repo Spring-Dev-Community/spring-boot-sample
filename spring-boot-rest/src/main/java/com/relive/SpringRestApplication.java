e393103475b22f404fd06d75144c608efb9372cb
relive <relive27@163.com>
2022-06-16 12:34:54 +0800

    Init spring-boot-rest sample

---
+++ spring-boot-rest/src/main/java/com/relive/SpringRestApplication.java
@@ -0,0 +1,17 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2022/5/17 12:51 下午
+ */
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
+}
