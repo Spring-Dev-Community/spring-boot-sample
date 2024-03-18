6ef7f912d88a9cf8b5c2a728a73bf4e74c77fd1c
ReLive27 <re_live27@163.com>
2024-03-18 21:11:03 +0800

    Update README.md

--- spring-boot-persistence/flyway/README.md
+++ spring-boot-persistence/flyway/README.md
@@ -3,3 +3,4 @@
 - [使用 Flyway 进行数据库迁移]()
 - [Spring Boot Flyway 多数据源配置]()
 - [Flyway 回调事件]()
+- [Spring Boot 结合 Flyway 从csv文件中插入数据]()
---
+++ spring-boot-persistence/flyway/src/main/java/com/relive/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ReLive
+ * @date: 2023/3/7 19:57
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
