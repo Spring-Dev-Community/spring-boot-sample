766a9fe24482d3f44f35e9e4a65ac7cec2053ede
ReLive27 <re_live27@163.com>
2024-04-21 21:43:24 +0800

    Upgrade Spring AI to stable version

--- spring-ai/src/main/java/com/relive27/ollama/controller/FictionController.java
+++ spring-ai/src/main/java/com/relive27/ollama/controller/FictionController.java
@@ -2,8 +2,8 @@ package com.relive27.ollama.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.ai.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +24,7 @@ public class FictionController {
     @GetMapping("/top/fiction")
     public String topCodeLanguage() {
         String message = "2023最受欢迎小说是什么";
-        return chatClient.generate(message);
+        return chatClient.call(message);
     }
 
     @GetMapping("/top/fiction/{year}")
@@ -32,6 +32,6 @@ public class FictionController {
         String message = "{year}最受欢迎小说是什么";
         PromptTemplate promptTemplate = new PromptTemplate(message);
         promptTemplate.add("year", year);
-        return chatClient.generate(promptTemplate.render());
+        return chatClient.call(promptTemplate.render());
     }
 }
--- spring-ai/src/main/java/com/relive27/openai/controller/CodeController.java
+++ spring-ai/src/main/java/com/relive27/openai/controller/CodeController.java
@@ -3,10 +3,10 @@ package com.relive27.openai.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.ChatResponse;
-import org.springframework.ai.openai.client.OpenAiChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.ai.parser.BeanOutputParser;
-import org.springframework.ai.prompt.Prompt;
-import org.springframework.ai.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +27,7 @@ public class CodeController {
     @GetMapping("/top/code/language")
     public String topCodeLanguage() {
         String message = "2023最受欢迎编程语言是什么";
-        return chatClient.generate(message);
+        return chatClient.call(message);
     }
 
     @GetMapping("/top/code/language/{year}")
@@ -35,7 +35,7 @@ public class CodeController {
         String message = "{year}最受欢迎编程语言是什么";
         PromptTemplate promptTemplate = new PromptTemplate(message);
         promptTemplate.add("year", year);
-        return chatClient.generate(promptTemplate.render());
+        return chatClient.call(promptTemplate.render());
     }
 
     @GetMapping("/top/code/language/{year}/json")
@@ -50,8 +50,8 @@ public class CodeController {
         log.info("FORMAT STRING: " + parser.getFormat());
 
         Prompt prompt = promptTemplate.create();
-        ChatResponse chatResponse = chatClient.generate(prompt);
-        String content = chatResponse.getGeneration().getContent();
+        ChatResponse chatResponse = chatClient.call(prompt);
+        String content = chatResponse.getResult().getOutput().getContent();
         return parser.parse(content);
     }
 }
